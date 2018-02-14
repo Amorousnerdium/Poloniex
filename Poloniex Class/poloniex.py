93b889935d1be19d3eb2d3b726fd04a59e5480d0
Amorousnerdium <jason.woodcock@gmail.com>
2018-02-14 14:31:28 -0500

    Multiple updates and code refactoring. Started adding proper docstrings as well.
    Close to usable... Close...

--- Poloniex Class/poloniex.py
+++ Poloniex Class/poloniex.py
@@ -1,17 +1,17 @@
 #!python3
 
 import abc
-import decimal
 import hmac
-from hashlib import sha512
 import json
 import time
+import urllib.request
+from decimal import Decimal
+from hashlib import sha512
 from urllib.error import URLError, HTTPError, ContentTooShortError
 from urllib.parse import urlencode
-import urllib.request
 
 
-class Exchange(metaclass=abc.ABCMeta)
+class Exchange(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def initialize(self):
@@ -42,7 +42,7 @@ class Exchange(metaclass=abc.ABCMeta)
         raise NotImplemented()
 
     @abc.abstractmethod
-    def cancel_order(self, currency_pair, order_number):
+    def cancel_order(self, order_number):
         raise NotImplemented()
 
     @abc.abstractmethod
@@ -62,29 +62,30 @@ class Exchange(metaclass=abc.ABCMeta)
 
 class Poloniex(Exchange):
     """
-        Hi! I am the Poloniex.com Cryptocurrency Exchange Class for Python >3.5! My life long goal
-        and penultimate aspiration is to encapsulate all published interactions for Poloniex's
-        trading API! Hopefully I will live long and prosper...
+        Hi! I am the Poloniex.com Cryptocurrency Exchange Class for Python >3.5! My life long goal and penultimate
+        aspiration is to encapsulate all published interactions for Poloniex's trading API! Hopefully I will live long
+        and prosper...
 
         Args:
-            api (str): API Access Key from Poloniex for authorization
+            api_key (str): API Access Key from Poloniex for authorization
             secret_key (str): Secret Key from Poloniex for authentication
             auto_init (bool): Launches the initialize() method upon instance creation if True
 
         Attributes:
-            api (str): Instance level storage for the API Access Key
+            api_key (str): Instance level storage for the API Access Key
             secret_key (str): Instance level storage for the Secret Key
-            connection (bool): Instance level indicator of successful connection
+            connection (bool): Instance level indicator of successful initialization
 
     """
+    rate_timer = []
+
 # Internal Class Methods
 
-    def __init__(self, api: str, secret_key: str, auto_init=False):
-        self.api_key = api
+    def __init__(self, api_key: str, secret_key: str, auto_init=False):
+        self.api_key = api_key
         self.secret_key = secret_key
         self.public_api = 'https://poloniex.com/public?command='
         self.trading_api = 'https://poloniex.com/tradingapi'
-        self.rate_timer = []
         if auto_init:
             self.connection = self.initialize()
         else:
@@ -100,10 +101,29 @@ class Poloniex(Exchange):
     def __bool__(self):
         return self.connection
 
-    def post_processing(self, data) -> dict:
-        pass
+    def initialize(self) -> bool:
+        """
+        Initiates a connection to the Poloniex API to verify correct API & Secret keys. Utilizes the balances() method's
+        return for determination.
+
+        Returns:
+            bool
+        """
+        if self.balances():
+            self.connection = True
+            return True
+        else:
+            self.connection = False
+            return False
 
     def rate_limit(self) ->bool:
+        """
+        Rate limiting method to prevent violation of Poloniex's policy for the number of requests per second. While
+        Poloniex has set the rate at 6 requests per second, this function limits the number of requests to 5 per second.
+
+        Returns:
+            bool
+        """
         self.rate_timer.append(time.time())
         if len(self.rate_timer) > 4:
             diff = self.rate_timer[4] - self.rate_timer[0]
@@ -113,14 +133,26 @@ class Poloniex(Exchange):
         return True
 
     def public_query(self, req: str, retries: int = 2) -> dict:
+        """
+        Builds and passes the actual request to Poloniex's public API. In the event of a 500 series http error, the
+        request is retried after 3 seconds up the maximum number of times defined by the 'retries' argument.
+        Args:
+            req (str): The actual request to be appended to the public API's url.
+            retries (int): Maximum number of retries in the event of a 500 series HTTP error. Default = 2
+
+        Returns:
+            dict
+        """
         url = self.public_api+req
         request = urllib.request.Request(url)
+        self.rate_limit()
         try:
             with urllib.request.urlopen(request) as response:
                 data = response.read()
         except (URLError, HTTPError, ContentTooShortError) as err:
             if retries > 0:
                 if hasattr(err, 'code') and 500 <= err.code < 600:
+                    time.sleep(3)
                     return self.public_query(req, retries - 1)
                 else:
                     return dict([])
@@ -129,17 +161,29 @@ class Poloniex(Exchange):
         return json.loads(data)
 
     def signed_query(self, req: dict, retries: int = 2) -> dict:
+        """
+        Builds and passes the actual request to Poloniex's trading API. In the event of a 500 series http error, the
+        request is retried after 3 seconds up the maximum number of times defined by the 'retries' argument.
+        Args:
+            req (dict): The actual request in JSON format.
+            retries (int): Maximum number of retries in the event of a 500 series HTTP error. Default = 2
+
+        Returns:
+            dict
+        """
         req['nonce'] = int(time.time()*1000)
         data = urlencode(req)
         sign = hmac.new(self.secret_key, data, sha512).hexdigest()
         headers = dict(Sign=sign, Key=self.api_key)
         url_req = urllib.request.Request(self.trading_api, data, headers)
+        self.rate_limit()
         try:
             with urllib.request.urlopen(url_req) as response:
                 data = response.read()
         except (URLError, HTTPError, ContentTooShortError) as err:
             if retries > 0:
                 if hasattr(err, 'code') and 500 <= err.code < 600:
+                    time.sleep(3)
                     return self.signed_query(req, retries - 1)
                 else:
                     return dict([])
@@ -148,13 +192,18 @@ class Poloniex(Exchange):
 
         return json.loads(data)
 
-    def initialize(self) -> bool:
-        if self.balances():
-            return True
-        else:
-            return False
-
     def update_keys(self, api: str, secret_key: str, auto_init: bool=False) -> bool:
+        """
+        Updates the API and Secret keys for the instance.
+
+        Args:
+            api (str): Poloniex Account API key
+            secret_key (str): Poloniex Account Secret key
+            auto_init (bool): Automatically launch the initialize() method to verify keys.
+
+        Returns:
+            bool
+        """
         self.api_key = api
         self.secret_key = secret_key
         if auto_init:
@@ -165,20 +214,51 @@ class Poloniex(Exchange):
 
     # Public API Methods
 
+    def chart_data(self):
+        """
+        Returns candlestick chart data. Required GET parameters are "currencyPair", "period" (candlestick period in
+        seconds; valid values are 300, 900, 1800, 7200, 14400, and 86400), "start", and "end". "Start" and "end" are
+        given in UNIX timestamp format and used to specify the date range for the data returned.
+
+        Sample output:
+            [
+                {   "date":1405699200,
+                    "high":0.0045388,
+                    "low":0.00403001,
+                    "open":0.00404545,
+                    "close":0.00427592,
+                    "volume":44.11655644,
+                    "quoteVolume":10259.29079097,
+                    "weightedAverage":0.00430015},
+            ...]
+
+        Returns:
+            dict
+        """
+        pass
+
+    def ticker_data(self):
+        """
+
+        Returns:
+            dict
+        """
+        pass
+
     # Trading Api Methods
 
     def balances(self) -> dict:
         request = dict(command='returnBalances')
         return self.signed_query(request)
 
-    def buy(self, currency_pair: str, rate: decimal.Decimal, amount: decimal.Decimal) -> dict:
+    def buy(self, currency_pair: str, rate: Decimal, amount: Decimal) -> dict:
         request = dict(command='buy',
                        currencyPair=currency_pair,
                        rate=rate,
                        amount=amount)
         return self.signed_query(request)
 
-    def sell(self, currency_pair: str, rate: decimal.Decimal, amount: decimal.Decimal) -> dict:
+    def sell(self, currency_pair: str, rate: Decimal, amount: Decimal) -> dict:
         request = dict(command='sell',
                        currencyPair=currency_pair,
                        rate=rate,
@@ -200,7 +280,7 @@ class Poloniex(Exchange):
                        currencyPair=currency_pair)
         return self.signed_query(request)
 
-    def withdraw(self, currency: str, address: str, amount: decimal.Decimal) -> dict:
+    def withdraw(self, currency: str, address: str, amount: Decimal) -> dict:
         request = dict(command='withdraw',
                        currency=currency,
                        amount=amount,
@@ -216,10 +296,14 @@ class Account:
         if self.exchange:
             self.balances = self.exchange.balances()
             self.trade_history = self.exchange.trade_history
-            self.orders = self.exchange.open_orders()
+            self.orders = self.exchange.open_orders(currency_pair='all')
 
 
 class CurrencyPair:
     def __init__(self, base, target):
         self.base_ticker = base
         self.target_ticker = target
+        self.base_name
+        self.target_name
+        self.string
+        self.price
